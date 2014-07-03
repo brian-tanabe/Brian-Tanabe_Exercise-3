18e41f9fc9dbd7ab3b964f0fdab8f6f2604f9cf8
Brian Tanabe <btanabe2@gmail.com>
2014-07-03 16:40:26 -0500

    SpiralArrayPopulator calculates starting index correctly.  Tests pass.

--- src/main/java/com/tanabe/rs/spiralarray/SpiralArrayPopulator.java
+++ src/main/java/com/tanabe/rs/spiralarray/SpiralArrayPopulator.java
@@ -5,14 +5,21 @@ package com.tanabe.rs.spiralarray;
  */
 public class SpiralArrayPopulator {
     protected int numberToPrintUpTo;
+    protected SpiralArray spiralArray;
 
     public SpiralArrayPopulator(int numberToPrintUpTo){
         this.numberToPrintUpTo = numberToPrintUpTo;
+        spiralArray = new SpiralArray(numberToPrintUpTo);
     }
+
+    protected SpiralArrayPopulator(){}
 
     public SpiralArray createAndPopulateSpiralArray(){
 
-
         return null;
+    }
+
+    protected static int determineStartingIndices(SpiralArray array){
+        return (int)(Math.ceil(Math.sqrt(array.getArrayHeight())) - 1);
     }
 }
--- src/test/java/com/tanabe/test/SpiralArrayPopulatorTest.java
+++ src/test/java/com/tanabe/test/SpiralArrayPopulatorTest.java
@@ -9,11 +9,36 @@ import static junit.framework.Assert.assertEquals;
 /**
  * Created by Brian on 7/3/2014.
  */
-public class SpiralArrayPopulatorTest {
+public class SpiralArrayPopulatorTest extends SpiralArrayPopulator {
 
     @Test
     public void checkSpiralArrayValuesForZero(){
         SpiralArray testSpiralArray = new SpiralArrayPopulator(0).createAndPopulateSpiralArray();
         assertEquals(0, testSpiralArray.getSpiralArray()[0][0]);
+    }
+
+    @Test
+    public void testDetermineStartingIndicesForZero(){
+        assertEquals(0, SpiralArrayPopulator.determineStartingIndices(new SpiralArray(0)));
+    }
+
+    @Test
+    public void testDetermineStartingIndicesForOne(){
+        assertEquals(0, SpiralArrayPopulator.determineStartingIndices(new SpiralArray(1)));
+    }
+
+    @Test
+    public void testDetermineStartingIndicesForTwo(){
+        assertEquals(0, SpiralArrayPopulator.determineStartingIndices(new SpiralArray(2)));
+    }
+
+    @Test
+    public void testDetermineStartingIndicesForThree(){
+        assertEquals(0, SpiralArrayPopulator.determineStartingIndices(new SpiralArray(3)));
+    }
+
+    @Test
+    public void testDetermineStartingIndicesForFour(){
+        assertEquals(1, SpiralArrayPopulator.determineStartingIndices(new SpiralArray(4)));
     }
 }

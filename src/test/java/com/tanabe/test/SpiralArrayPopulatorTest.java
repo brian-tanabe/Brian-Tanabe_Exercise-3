c6cf5cf5da54f97dcb3de1d6bff2d04103f6efda
Brian Tanabe <btanabe2@gmail.com>
2014-07-03 19:37:17 -0500

    SpiralArrayPopulator for inputs: 0, 1, 2, 3, 4, 8, 24 now pass.

--- src/test/java/com/tanabe/test/SpiralArrayPopulatorTest.java
+++ src/test/java/com/tanabe/test/SpiralArrayPopulatorTest.java
@@ -65,6 +65,36 @@ public class SpiralArrayPopulatorTest extends SpiralArrayPopulator {
         assertEquals("eight placement", 8, testSpiralArray.getSpiralArray()[2][0]);
     }
 
+    @Test
+    public void checkSpiralArrayValuesForTwentyFour(){
+        SpiralArray testSpiralArray = new SpiralArrayPopulator(24).createAndPopulateSpiralArray();
+        assertEquals("zero placement", 0, testSpiralArray.getSpiralArray()[2][2]);
+        assertEquals("one placement", 1, testSpiralArray.getSpiralArray()[3][2]);
+        assertEquals("two placement", 2, testSpiralArray.getSpiralArray()[3][3]);
+        assertEquals("three placement", 3, testSpiralArray.getSpiralArray()[2][3]);
+        assertEquals("four placement", 4, testSpiralArray.getSpiralArray()[1][3]);
+        assertEquals("five placement", 5, testSpiralArray.getSpiralArray()[1][2]);
+        assertEquals("six placement", 6, testSpiralArray.getSpiralArray()[1][1]);
+        assertEquals("seven placement", 7, testSpiralArray.getSpiralArray()[2][1]);
+        assertEquals("eight placement", 8, testSpiralArray.getSpiralArray()[3][1]);
+        assertEquals("nine placement", 9, testSpiralArray.getSpiralArray()[4][1]);
+        assertEquals("ten placement", 10, testSpiralArray.getSpiralArray()[4][2]);
+        assertEquals("eleven placement", 11, testSpiralArray.getSpiralArray()[4][3]);
+        assertEquals("twelve placement", 12, testSpiralArray.getSpiralArray()[4][4]);
+        assertEquals("thirteen placement", 13, testSpiralArray.getSpiralArray()[3][4]);
+        assertEquals("fourteen placement", 14, testSpiralArray.getSpiralArray()[2][4]);
+        assertEquals("fifteen placement", 15, testSpiralArray.getSpiralArray()[1][4]);
+        assertEquals("sixteen placement", 16, testSpiralArray.getSpiralArray()[0][4]);
+        assertEquals("seventeen placement", 17, testSpiralArray.getSpiralArray()[0][3]);
+        assertEquals("eighteen placement", 18, testSpiralArray.getSpiralArray()[0][2]);
+        assertEquals("nineteen placement", 19, testSpiralArray.getSpiralArray()[0][1]);
+        assertEquals("twenty placement", 20, testSpiralArray.getSpiralArray()[0][0]);
+        assertEquals("twenty-one placement", 21, testSpiralArray.getSpiralArray()[1][0]);
+        assertEquals("twenty-two placement", 22, testSpiralArray.getSpiralArray()[2][0]);
+        assertEquals("twenty-three placement", 23, testSpiralArray.getSpiralArray()[3][0]);
+        assertEquals("twenty-four placement", 24, testSpiralArray.getSpiralArray()[4][0]);
+    }
+
     //region determineIndicesTests
     @Test
     public void testDetermineStartingIndicesForZero(){
